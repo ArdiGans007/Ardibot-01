0a9ffc7aa88842590f861ef73bb9b6a0c7925eaa
ArdiGans <78136450+ArdiGans007@users.noreply.github.com>
2021-02-04 13:14:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let { spawn } = require('child_process')
 let path = require('path')
 let fs = require('fs')
 const CFonts  = require('cfonts')
-CFonts.say('TERMUX WHATSAPP BOT', {
+CFonts.say('ARDI WHATSAPP BOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say('\'Youtube:\' Drawl Nag', {
+CFonts.say('\'Youtube:\' ArdiGansYT', {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
